5e9eb0a56a86c0d130d44b5e0a53c4f0f714b936
jinyeop1214 <bb3598@gmail.com>
2022-08-23 21:46:21 +0900

    home, login, signup page generate, no state

---
+++ Components/Header/Header.tsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import { userAgent } from "next/server";
+import React, { useState } from "react";
+
+const header = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const handleLogOut = () => {};
+
+	return (
+		<div className="Header">
+			<div className="Bar">
+				{isLoggedIn ? (
+					<button className="logoutbtn" onClick={handleLogOut}>
+						log out
+					</button>
+				) : (
+					<>
+						<Link href="/login">
+							<a>Log In</a>
+						</Link>{" "}
+						|{" "}
+						<Link href="/signup">
+							<a>Sign Up</a>
+						</Link>
+					</>
+				)}
+			</div>
+			<div className="Logo">Book Manager</div>
+			<style jsx>
+				{`
+					a {
+						text-decoration: none;
+						color: midnightblue;
+						font-size: 17px;
+						font-weight: bold;
+						font-family: "Poppins";
+						line-height: 1.75em;
+						letter-spacing: -0.05em;
+					}
+
+					.Logo {
+						color: midnightblue;
+						margin-top: 10px;
+						text-align: center;
+						font-size: 32px;
+						font-weight: bold;
+						font-family: "Poppins";
+					}
+
+					.Bar {
+						display: inline-block;
+					}
+
+					.Header {
+						height: 100px;
+						padding: 20px;
+						padding-left: 30px;
+						padding-right: 30px;
+						text-align: end;
+						border-top: 1px solid darkblue;
+						border-bottom: 2px solid darkblue;
+					}
+
+					.logoutbtn {
+						background-color: white;
+						text-decoration: none;
+						color: midnightblue;
+						font-size: 16.5px;
+						font-weight: bold;
+						font-family: "Poppins";
+						padding: 0px;
+						line-height: 1.75em;
+						border: none;
+						letter-spacing: -0.05em;
+						cursor: pointer;
+					}
+				`}
+			</style>
+		</div>
+	);
+};
+
+export default header;
---
+++ pages/login.tsx
@@ -0,0 +1,147 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
+
+const login: NextPage = () => {
+	const [id, setId] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [wrongText, setWrongText] = useState<string>("");
+
+	const onChangeId = (e: ChangeEvent<HTMLInputElement>) => {
+		setId(e.target.value);
+		setWrongText("");
+	};
+
+	const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+		setWrongText("");
+	};
+
+	const onEnterDown = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.keyCode === 13) {
+			handleLogIn();
+		}
+	};
+
+	/**
+	 * TODO: 로그인 with DB
+	 * @returns
+	 */
+	const handleLogIn = () => {
+		if (id === "" || password === "") {
+			setWrongText("빈칸을 채워주세요.");
+			return;
+		}
+	};
+
+	return (
+		<div className="container">
+			<div className="title">
+				<Link href="/">
+					<a className="link">Book Manager</a>
+				</Link>
+			</div>
+			<div className="subtitle">Log In</div>
+			<input
+				className="id"
+				value={id}
+				type="text"
+				onChange={onChangeId}
+				placeholder="ID"
+			/>
+			<input
+				className="password"
+				value={password}
+				type="password"
+				onChange={onChangePassword}
+				placeholder="Password"
+				onKeyDown={onEnterDown}
+			/>
+			<div className="warning">{wrongText}</div>
+			<button className="btn" onClick={handleLogIn}>
+				{" "}
+				Log In{" "}
+			</button>
+			<style jsx>{`
+				.container {
+					max-width: 376px;
+					margin: 100px auto;
+					padding: 0 16px;
+					background-color: white;
+					box-sizing: border-box;
+					background: #fff;
+					border-radius: 10px;
+					box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
+					border: 1px solid rgb(221, 221, 221);
+				}
+
+				.title {
+					text-align: center;
+					color: midnightblue;
+					padding: 30px;
+					border-bottom: 2px solid midnightblue;
+					font-family: "Poppins";
+					letter-spacing: -0.02em;
+				}
+
+				.link {
+					font-size: 32px;
+				}
+
+				.subtitle {
+					text-align: center;
+					font-size: 30px;
+					color: midnightblue;
+					padding: 30px;
+					padding-top: 35px;
+					font-family: "Poppins";
+					letter-spacing: -0.02em;
+				}
+
+				.id {
+					display: block;
+					width: 94%;
+					margin: 10px;
+					font-size: 20px;
+					padding: 8px;
+					margin-bottom: 12px;
+					box-sizing: border-box;
+					border: 2px solid #c4c4c4;
+					border-radius: 10px;
+				}
+
+				.password {
+					display: block;
+					width: 94%;
+					margin: 10px;
+					margin-bottom: 15px;
+					font-size: 20px;
+					padding: 8px;
+					box-sizing: border-box;
+					border: 2px solid #c4c4c4;
+					border-radius: 10px;
+				}
+
+				.warning {
+					color: red;
+				}
+
+				.btn {
+					border-radius: 10px;
+					text-align: center;
+					font-size: 25px;
+					margin: 10px;
+					width: 94%;
+					padding: 8px;
+					border: 2px solid #c4c4c4;
+					margin-bottom: 35px;
+					cursor: pointer;
+					font-family: "Poppins";
+					letter-spacing: -0.02em;
+				}
+			`}</style>
+		</div>
+	);
+};
+
+export default login;
